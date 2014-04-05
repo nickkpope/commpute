c3800967b6fdb183bb45d7d320ffa588567ec51b
Ben <zygoth2@gmail.com>
2014-04-04 20:43:01 -0600

    added stubs to JobInformationAPI

--- JPPF/JPPF-4.0.1-application-template/src/org/jppf/application/template/JobInformationAPI.java
+++ JPPF/JPPF-4.0.1-application-template/src/org/jppf/application/template/JobInformationAPI.java
@@ -4,5 +4,27 @@
  */
 public class JobInformationAPI
 {
+	/**
+	* @Returns the ID of the Job that is created
+	*/
+	public String submitTestJob(String desiredOutput, int secondsLong)
+	{
 
+	}
+	
+	/*
+	* @Returns the ID of the Job that is created
+	*/
+	public void submitBlenderJob(String[] dependencies, String[] nodeIPs)
+	{
+
+	}
+
+	/*
+	* @Returns "submitted", "pending", "executing", "complete", or "failed"
+	*/
+	public String getJobStatus(String jobID)
+	{
+
+	}
 }
