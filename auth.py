806a828d8c17ddb31a864350118e354f5dd2fe7e
Peter Anderson <anderson.peter@byu.edu>
2014-04-05 13:48:21 -0600

    Twitter works!...with other people's keys

--- auth.py
+++ auth.py
@@ -1,76 +1,56 @@
 from flask import g, session
-from flask.ext.login import LoginManager, login_user, logout_user
-from flask.ext.login import current_user, login_required
-from commpute import app, login_manager
-from flask_oauthlib.client import OAuth
+from ops import app, login_manager, facebook, twitter
 
-#Oauth Setup
-oauth = OAuth(app)
 
-#Facebook
-facebook = oauth.remote_app('facebook',
-	base_url='https://graph.facebook.com',
-	request_token_url=None,
-	access_token_url='/oauth/access_token',
-	authorize_url='https://www.facebook.com/dialog/oauth',
-	consumer_key='5',
-	consumer_secret='5'
-)
-
-@facebook_tokengetter
+@facebook.facebook_tokengetter
 def get_facebook_token():
-	if 'facebook_oauth' in session:
-		resp = session['facebook_oauth']
-		return resp['oauth_token'], resp['oauth_token_secret']
+    if 'facebook_oauth' in session:
+        resp = session['facebook_oauth']
+        return resp['oauth_token'], resp['oauth_token_secret']
 
-#Twitter
-twitter = oauth.remote_app('twitter',
-	base_url = 'https://api.twitter.com/1/',
-	request_token_url='https://api.twitter.com/oauth/request_token',
-	access_token_url='https://api.twitter.com/oauth/access_token',
-	authorize_url='https://api.twitter.com/oauth/authenticate',
-	consumer_key='FW0k326IPRWRB6cPd6AEw',
-	consumer_secret='OVLhEfVm5ZaU8XHPWaFosenmDm4o3uj0fDi3E0KfQ'
-)
 
-@twitter_tokengetter
+@twitter.twitter_tokengetter
 def get_twitter_token():
-	if 'twitter_oauth' in session:
-		resp = session['twitter_oauth']
-		return resp['oauth_token'], resp['oauth_token_secret']
+    if 'twitter_oauth' in session:
+        resp = session['twitter_oauth']
+        return resp['oauth_token'], resp['oauth_token_secret']
+
 
 @app.before_request
 def before_request():
-	g.user = None
-	if 'twitter_oauth' in session:
-		g.user = session['twitter_oauth']
+    g.user = None
+    if 'twitter_oauth' in session:
+        g.user = session['twitter_oauth']
 
 #Login Customization
 login_manager.login_view = 'login'
 login_manager.login_message = u'Please log in to access this page'
 
+
 @login_manager.user_loader
 def load_user(userid):
-	return User('John')
+    return User('John')
+
 
 class User:
 
-	def __init__(self, name):
-		self.name = name
-		self.authenticated = True		
+    def __init__(self, name):
+        self.name = name
+        self.authenticated = True
 
-#Determines whether a user has provided the correct crudentials
-	def	is_authenticated(self):
-		return self.authenticated
+    def is_authenticated(self):
+        '''Determines whether a user has provided the correct crudentials'''
+        return self.authenticated
 
-#Determines whether a user is an active user i.e. not suspended
-	def is_active(self):
-		return True
-		
-#Determines whether the user is anonymous (real users should return false)
-	def is_anonymous(self):
-		return false
+    def is_active(self):
+        '''Determines whether a user is an active user i.e. not suspended'''
+        return True
 
-#Return the id of the user (the id must be in unicode)
-	def get_id(self):
-		return u'5'
+    def is_anonymous(self):
+        '''Determines whether the user is anonymous
+        (real users should return false)'''
+        return False
+
+    # Return the id of the user (the id must be in unicode)
+    def get_id(self):
+        return u'5'
--- commpute.py
+++ commpute.py
@@ -1,98 +1,90 @@
 # all the imports
-import os
-from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
-from flask.ext.login import LoginManager, login_required, login_user
-from auth import *
-# create our little application :)
-app = Flask(__name__)
-app.config.from_object(__name__)
-login_manager = LoginManager()
-login_manager.init_app(app)
-
-
-# Load default config and override config from an environment variable
-app.config.update(dict(
-    DATABASE=os.path.join(app.root_path, 'flaskr.db'),
-    DEBUG=True,
-    SECRET_KEY='development key',
-    USERNAME='admin',
-    PASSWORD='default'
-))
-app.config.from_envvar('FLASKR_SETTINGS', silent=True)
+from flask import session, redirect, url_for, render_template, flash, request
+from flask.ext.login import login_required, logout_user
+from ops import app, facebook, twitter
 
 
 @app.route('/')
 def show_landing():
     return render_template('landing.html')
 
+
 @app.route('/login')
 def login():
-	'''
-	form = LoginForm()
-	if form.validate_on_submit():
-		#validate user here
-		user = User('John')
-		login_user(user)
-		flash("Logged in successfully.")
-		return redirect(url_for('profile', username='John'))
-	'''
-	return render_template("login.html")
+
+    # form = LoginForm()
+    # if form.validate_on_submit():
+    #   #validate user here
+    #   user = User('John')
+    #   login_user(user)
+    #   flash("Logged in successfully.")
+    #   return redirect(url_for('profile', username='John'))
+
+    return render_template("login.html")
 
 
 @app.route('/signup')
 def sign_up():
-	return 'Sign Up'
+    return 'Sign Up'
+
 
 @app.route('/docs')
 def docs():
-	return 'Docs'
+    return 'Docs'
+
 
 @app.route('/testdrive')
 def test_drive():
-	return 'Test Drive'
+    return 'Test Drive'
 
-#@login_required
+
 @app.route('/profile/<username>')
 def profile(username):
-	return 'Welcome %s to your profile page!' % username
+    return 'Welcome %s to your profile page!' % username
+
 
 @login_required
 @app.route('/logout')
 def logout():
-	logout_user()
-	return redirect('/')
+    logout_user()
+    return redirect('/')
+
 
 @app.route('/facebook')
-def facebook():
-	callback_url = url_for('facebook-auth')
-	return facebook.authorize(callback=callback_url)
+def facebook_login():
+    callback_url = url_for('facebook_auth')
+    return facebook.authorize(callback=callback_url, next=request.args.get('next'))
 
-@app.route('/facebook-auth')
+
+@app.route('/facebook_auth')
 @facebook.authorized_handler
 def facebook_auth(resp):
-	if resp is None:
-		flash('You denied the request to sign in.')
-	else:
-		session['facebook_oauth'] = resp
-	return redirect(url_for('profile', username='Facebook'))
+    if resp is None:
+        flash('You denied the request to sign in.')
+    else:
+        session['facebook_oauth'] = resp
+    return redirect(url_for('profile', username='Facebook'))
+
 
 @app.route('/twitter')
-def twitter():
-	callback_url = url_for('twitter-auth')
-	return twitter.authorize(callback=callback_url)
+def twitter_login():
+    callback_url = url_for('twitter_auth', next=request.args.get('next'))
+    return twitter.authorize(callback=callback_url)
 
-@app.route('/twitter-auth')
+
+@app.route('/twitter_auth')
 @twitter.authorized_handler
 def twitter_auth(resp):
-	if resp is None:
-		flash('You denied the request to sign in.')
-	else:
-		session['twitter_oauth'] = resp
-	return redirect(url_for('profile', username='Twitter'))
+    if resp is None:
+        flash('You denied the request to sign in.')
+    else:
+        session['twitter_oauth'] = resp
+    return redirect(url_for('profile', username='Twitter'))
+
 
 @app.route('/google')
 def google():
-	return 'Google'
+    return 'Google'
 
 
 if __name__ == '__main__':
