e23eda90143489ed8b65a36c2327b6c50311efc7
Ben <zygoth2@gmail.com>
2014-04-07 20:45:40 -0600

    Apache is the worst!!!!!!!!

--- JPPF/JPPF-4.0.1-application-template/src/org/jppf/application/template/Main.java
+++ JPPF/JPPF-4.0.1-application-template/src/org/jppf/application/template/Main.java
@@ -1,12 +1,37 @@
 package org.jppf.application.template;
 
-import org.jppf.application.template.JobInformationAPITester;
+import org.jppf.application.template.JobInformationAPI;
+
+import java.net.InetAddress; 
+
+import org.apache.xmlrpc.common.TypeConverterFactoryImpl;
+import org.apache.xmlrpc.server.XmlRpcServer; 
+import org.apache.xmlrpc.server.XmlRpcServerConfigImpl; 
+import org.apache.xmlrpc.webserver.WebServer; 
+import org.apache.xmlrpc.server.*;
 
 public class Main
 {	
-	public static void main(String[] args) 
+	public static void main(String[] args) throws Exception
 	{
-		JobInformationAPITester.testJobInformationAPI();
+
+		WebServer webServer = new WebServer(8080, InetAddress.getByName("127.0.0.1")); 
+        
+        XmlRpcServer xmlRpcServer = webServer.getXmlRpcServer(); 
+        
+        /*DynamicHandlerMapping dhm = new DynamicHandlerMapping(new TypeConverterFactoryImpl(), true); 
+    
+        // add "Calculator" handler - used by regular agent 
+        dhm.addHandler("jobs", JobInformationAPI.class);
+        xmlRpcServer.setHandlerMapping(dhm); 
+        
+        XmlRpcServerConfigImpl serverConfig = (XmlRpcServerConfigImpl) xmlRpcServer.getConfig(); 
+        serverConfig.setEnabledForExtensions(true); 
+        serverConfig.setContentLengthOptional(false);
+*/
+        webServer.start(); 
+
+		//JobInformationAPITester.testJobInformationAPI();
 		
 		//System.exit(0);
 	}
